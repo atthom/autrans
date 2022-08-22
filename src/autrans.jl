a75a0dfb1b0046d17b525efcd4ca034a0fadc9fb
atthom <thom.jalabert@gmail.com>
2022-08-22 20:59:13 +0200

    add pers_per_work parameter into fitness function

--- src/autrans.jl
+++ src/autrans.jl
@@ -7,14 +7,14 @@ using StatsBase
 using Distributions
 using PrettyTables
 
-function fitness(schedule, verbose=false)
+function fitness(schedule, pers_per_work, verbose=false)
     per_worker = sum(schedule, dims=1)
     per_job = sum(schedule, dims=2)
    
-    balance = maximum(per_worker) - minimum(per_worker)
-    job_size = fill(2, length(per_job))
+    #balance = maximum(per_worker) - minimum(per_worker)
+    job_size = fill(pers_per_work, length(per_job))
     balanced_work = (per_job .- job_size).^2
-    balanced_work = 1000 * sum(balanced_work)
+    balanced_work = 500 * sum(balanced_work)
 
     spread = @chain schedule begin
         cumsum(_, dims=1)
@@ -26,36 +26,14 @@ function fitness(schedule, verbose=false)
     end
 
     if verbose
-        println("$balanced_work, $(balance^2), $spread")
+        println("$balanced_work, $spread")
     end
 
-    return -balanced_work -balance^2  -2*spread
+    return -balanced_work -2*spread
 end
 
 rand_mutate(schedule, p_mutate) = schedule .⊻ rand(Bernoulli(p_mutate), size(schedule))
 
-function rand_permute(schedule)
-    nz_schedule = findall(!iszero, schedule)
-    if length(nz_schedule) < 2
-        return schedule
-    end
-    selected = sample(nz_schedule, 2)
-
-    rev_col = @chain selected begin
-        Tuple.(_)
-        zip(_...)
-        collect
-        reverse(_[1]), _[2]
-        zip(_...)
-        collect
-        CartesianIndex.(_)
-    end
-    p_schedule = copy(schedule)
-    p_schedule[selected] .= false
-    p_schedule[rev_col] .= true
-
-    return p_schedule
-end
 
 function permute(schedule)
     new_schedule = copy(schedule)
@@ -71,28 +49,18 @@ function permute(schedule)
     return new_schedule
 end
 
-function mutate(schedule, p_mutate)
-    if rand() < 0.3
-        return rand_mutate(schedule, p_mutate)
-    else
-        return rand_permute(schedule)
-    end
-end
-
-populate(schedule, p_mutate, nb) = [mutate(schedule, p_mutate) for i in 1:nb]
-
 min_max(v) = (v .- minimum(v)) / (maximum(v) - minimum(v))
+
 standard(v) = (v .- minimum(v)) / std(v)
 
-selection(schedules, pop_min) = @chain schedules fitness.(_) standard wsample(schedules, _, pop_min)
+selection(schedules, pers_per_work, pop_min) = @chain schedules fitness.(_, pers_per_work) standard wsample(schedules, _, pop_min)
 
-function generation(schedules, p_mutate, pop_min, pop_max)
-    selected = selection(schedules, pop_min)
+function generation(schedules, pers_per_work, pop_min, pop_max)
+    selected = selection(schedules, pers_per_work, pop_min)
 
     return @chain selected begin
         sample(_, pop_max - pop_min, replace=true)
-        mutate.(_, p_mutate)
-        #permute.(_)
+        permute.(_)
         vcat(selected, _)
     end
 end
@@ -139,17 +107,12 @@ end
 
 function first_generation(days, task_per_day, nb_workers, pers_per_work, pop_max)
     schedule1 = fill(false, (days*task_per_day, nb_workers))
-
-    for i in 1:days*task_per_day
-        schedule1[1:pers_per_work] .= true
-    end
+    schedule1[1:pers_per_work, :] .= true
     return [permute(schedule1) for i in 1:pop_max]
 end
 
 function find_schedule(workers, days; pers_per_work=2, task_per_day=5, nb_generation = 5000, p_mutate = 0.01, pop_min = 500, pop_max = 1000)
     nb_workers = length(workers)
-    #schedule1 = fill(false, (days*task_per_day, nb_workers))
-    #schedules = [schedule1, schedule1 .+ true]
     schedules = first_generation(days, task_per_day, nb_workers, pers_per_work, pop_max)
     
     iter = ProgressBar(1:nb_generation)
@@ -162,7 +125,7 @@ function find_schedule(workers, days; pers_per_work=2, task_per_day=5, nb_genera
             println("max == median population, early stopping")
             break
         end
-        schedules = generation(schedules, p_mutate, pop_min, pop_max)
+        schedules = generation(schedules, pers_per_work, pop_min, pop_max)
     end
 
     return @chain schedules sort(_, by=fitness, rev=true) pprint(_[1], workers, days)
