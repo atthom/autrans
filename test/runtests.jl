3444fb14a7386a73dd84bf415ac10f66fa00d1db
thomas <thom.jalabert@gmail.com>
2023-06-03 22:21:23 +0200

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,19 @@
 using Test
-
 using Autrans
 
-
+function quick_test(days, n1, n2, workers, shouldbe)
+    schedule = SmallSchedule(days, n1, n2, workers)
+    result = optimize(schedule)
+    @info make_df(schedule, result)
+    @test fitness(result, schedule) <= shouldbe
+end
 
 @testset "test_exact" begin
-    nb_days = 7
-    nb_task_per_day = 5
-    nb_pers_per_work = 1
-    #workers = ["thomas", "chronos", "curt", "astor", "manal", "thibs", "laura", "benj"]
-    workers = ["Cookie", "Fish", "Chronos"]
-    schedule = SmallSchedule(nb_days, nb_task_per_day, nb_pers_per_work, workers)
-    @info schedule
-    result = optimize(x -> fitness(x, schedule), schedule)
-    @info fitness(result, schedule, true))
-    pprint(schedule, result)
+    quick_test(7, 2, 2,  ["Cookie", "Fish"], 0)
 
+    if false
+        quick_test(7, 1, 1,  ["Cookie"], 0)
+        quick_test(7, 2, 1,  ["Cookie", "Fish"], 10)
+        quick_test(7, 1, 2,  ["Cookie", "Fish"], 0)
+    end
 end
