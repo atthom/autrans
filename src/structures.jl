eea94e0534c523781cc8655b0760f52220a4404f
atthom <thom.jalabert@gmail.com>
2023-06-06 00:20:27 +0200

    add cardinality and permutation space helper

--- src/structures.jl
+++ src/structures.jl
@@ -35,4 +35,24 @@ SmallSchedule(days::Int, task_per_day::Int, worker_per_work::Int, workers::Vecto
 
 
 Searchpath(s::SmallSchedule) = BitArraySpace(s.days*s.task_per_day*s.nb_workers)
+function SearchpathPermutation(s::SmallSchedule) 
+    number_of_slots = s.days*s.task_per_day*s.worker_per_work
+    true_slots = fill(true, number_of_slots)
+    false_slots = fill(false, s.days*s.task_per_day*s.nb_workers - number_of_slots)
+    return PermutationSpace(vcat(true_slots, false_slots))
+end
+
 Base.reshape(result, s::SmallSchedule) = reshape(result, (s.days*s.task_per_day, s.nb_workers))
+
+
+cardinality(s::SmallSchedule) = BigInt(2)^(s.days*s.task_per_day*s.nb_workers)
+
+
+
+
+function cardinality2(s::SmallSchedule) 
+    n = BigInt(s.days*s.task_per_day*s.nb_workers)
+    k = BigInt(s.days*s.task_per_day*s.worker_per_work)
+    @info n, k
+    return factorial(n) / (factorial(k)*factorial(n - k))
+end
