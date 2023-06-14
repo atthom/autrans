e1a9f6f932154b2e124e7e1f6dc2da2c3ecc564f
atthom <thom.jalabert@gmail.com>
2023-06-14 21:36:45 +0200

    update dependencies

--- src/Autrans.jl
+++ src/Autrans.jl
@@ -3,16 +3,21 @@ module Autrans
 using Stipple, StippleUI
 using DataFrames
 using Metaheuristics
-using Distributions: maximum
 using Chain
-using PrettyTables
+using Combinatorics
 
-export SmallSchedule, fitness, pprint, optimize, make_df
+export SmallSchedule, fitness, optimize, find_schedule, search_space, cardinality, julia_main
 
 
 include("structures.jl")
 include("core.jl")
 
 include("autrans_ui.jl")
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    julia_main()
+end
+
 
 end
