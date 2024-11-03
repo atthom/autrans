0ec6e6cc74d2f1c02ec0759cd0466ec6bd894680
atthom <thom.jalabert@gmail.com>
2024-11-03 21:25:30 +0100

    add

--- src/AutransUI.py
+++ src/AutransUI.py
@@ -4,7 +4,18 @@ import pandas as pd
 import numpy as np
 import requests
 
-
+if True:
+    st.html(
+"""
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5TZ38RNJ');</script>
+<!-- End Google Tag Manager -->
+"""
+    )
 
 
 @st.dialog("Schedule is not possible")
@@ -185,4 +196,13 @@ if submit:
     else:
         sat_schedule(sat_agg["msg"])
         
-        
+
+if True:
+    st.html(
+"""
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5TZ38RNJ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+"""
+    )
